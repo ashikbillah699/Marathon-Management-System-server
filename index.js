72acd0c7b4f42f1b0a73a7237ab474239d9b8849
ashikbillah699 <ashikbillah699@gmail.com>
2025-01-06 15:15:56 +0600

    delete single marathon from database

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
         res.send(result);
     })
 
+    // delete single marathon from database
+    app.delete('/marathon/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await marathonCollection.deleteOne(query);
+        res.send(result);
+    })
+
    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
