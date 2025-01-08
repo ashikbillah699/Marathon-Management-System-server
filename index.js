7f205d14c23881d84f8cb7fdb13ce0bd1f946fba
ashikbillah699 <ashikbillah699@gmail.com>
2025-01-08 20:57:15 +0600

    jwt impliment

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j8csd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,16 +26,66 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+// verify token 
+const verifyToken = (req, res, next)=>{
+    const token = req.cookies?.token;
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access!!'});
+    }
+    jwt.verify(token,process.env.SECRET_KEY, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access!!'});
+        }
+        req.user = decoded;
+    })
+
+    next()
+}
+
 async function run() {
     try {
         const marathonCollection = client.db('MarathosDB').collection('Marathons');
         const registrationCollection = client.db('MarathosDB').collection('ApplyMarathons');
 
+        // generate jwt
+        app.post('/jwt', async (req, res)=>{
+            const email = req.body;
+            const token = jwt.sign(email, process.env.SECRET_KEY, {expiresIn: '20d'});
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            })
+            .send({success: true});
+
+        })
+
+        // remove jwt
+        app.get('/logout', async(req, res)=>{
+            res
+            .clearCookie('token', {
+               maxAge: 0,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            })
+            .send({success: true})
+        })
+
         //get All Marathons
-        app.get('/marathons', async (req, res) => {
+        app.get('/marathons',verifyToken, async (req, res) => {
+            const decodedEmail = req.user?.email;
+            const email = req.query.email;
+            // console.log('decodedEmail: ', decodedEmail);
+            // console.log('Email : ', email);
+
+            if(decodedEmail !== email){
+                return res.status(403).send({message: "unauthorized access!!"});
+            };
+
             const sort = req.query.sort;
             let options = {createdAt: sort === 'asc' ? 1 : -1};
-            // if(sort) options = {sort: {createdAt: sort === 'asc' ? 1 : -1}}
             const result = await marathonCollection.find().sort(options).toArray();
             res.send(result);
         })
@@ -48,17 +105,34 @@ async function run() {
         })
 
         // get All Marathons posted by a specific user
-        app.get('/marathonsSpecific/:email', async (req, res) => {
+        app.get('/marathonsSpecific/:email',verifyToken, async (req, res) => {
+            const decodedEmail = req.user?.email;
             const email = req.params.email;
             const query = { "marathonCreater.email": email };
+            // console.log('decodeEmail', decodedEmail)
+            // console.log('parammail', decodedEmail)
+
+            if(decodedEmail !== email){
+                return res.status(403).send({message: 'unauthorized access!!'})
+            }
+
             const result = await marathonCollection.find(query).toArray();
             res.send(result);
         })
 
         // get All Marathons posted by a specific user
-        app.get('/registationsSpecific/:email', async (req, res) => {
+        app.get('/registationsSpecific/:email', verifyToken, async (req, res) => {
+            const decodedEmail = req.user?.email;
             const email = req.params.email;
             const search = req.query.search;
+
+            //  console.log('decodeEmail', decodedEmail)
+            // console.log('parammail', decodedEmail)
+
+            if(decodedEmail !== email){
+                return res.status(403).send({message: 'unauthorized access!!'})
+            }
+
             console.log( search)
             const query = { email: email , ...(search && { marathonTitle:{
                 $regex: search,
@@ -69,8 +143,16 @@ async function run() {
         })
 
         // save marathos data in db
-        app.post('/marathon', async (req, res) => {
+        app.post('/marathon',verifyToken, async (req, res) => {
+            const decodedEmaiil = req.user?.email;
             const marathonData = req.body;
+            const email = marathonData?.marathonCreater?.email;
+            // console.log('decodeEmail', decodedEmaiil)
+            // console.log('email:', email);
+
+            if(decodedEmaiil !== email){
+                return res.status(403).send({message: 'unauthorized access'})
+            }
             const result = await marathonCollection.insertOne(marathonData)
             // console.log(marathonData);
             res.send(result);
